d2dbba3693c88514b6a0a0e5315c9dce758013a9
Rahul Kumar Bhatt <bhattrahul270@gmail.com>
2023-12-31 09:35:08 +0530

    added routes and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import {
+  AboutPage,
+  ProfilePage,
+  HomePage,
+  SigninPage,
+  SignupPage,
+} from "./pages";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { HomePage } from "./home/Home";
+export { AboutPage } from "./about/About";
+export { ProfilePage } from "./profile/Profile";
+export { SigninPage } from "./auth/Sigin";
+export { SignupPage } from "./auth/Signup";
